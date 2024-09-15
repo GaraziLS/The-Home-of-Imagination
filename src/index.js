50f91b8661c0c5b22b519f8365e4c86c4ea7827f
GaraziLS <glabakasanabria@gmail.com>
2024-09-15 18:56:17 +0200

    added code to prevent render slowing down

--- src/index.js
+++ src/index.js
@@ -12,6 +12,23 @@ root.render(
   </HashRouter>
 );
 
+const url = `https://devcamp-fullstack-project-backend.onrender.com/`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+  function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+
+setInterval(reloadWebsite, interval);
+  
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
