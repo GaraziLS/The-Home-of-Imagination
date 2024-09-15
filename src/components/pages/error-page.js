33a47d18617d92bfa012ee8bb0914e985dbbb3d6
GaraziLS <glabakasanabria@gmail.com>
2024-09-15 16:21:00 +0200

    adjusted more paths

--- src/components/pages/error-page.js
+++ src/components/pages/error-page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
+import { NavLink } from 'react-router-dom';
 
 export default function () {
     return (
